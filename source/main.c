a464660a4960fb6f1f7732cb57cee779594cd31f
UnoDeaj05 <jdyamen@gmail.com>
2022-09-02 23:34:57 +1000

    made intro sprite respond to button press

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 // Images borrowed from:
 //   https://kenney.nl/assets/space-shooter-redux
 #include <citro2d.h>
+#include <3ds.h>
 
 #include <assert.h>
 #include <string.h>
@@ -17,7 +18,6 @@
 typedef struct
 {
 	C2D_Sprite spr;
-	float dx, dy; // velocity
 } Sprite;
 
 static C2D_SpriteSheet spriteSheet;
@@ -33,35 +33,35 @@ static void initSprites() {
 	C2D_SpriteFromSheet(&introSprite->spr, spriteSheet, 0);
 	C2D_SpriteSetCenter(&introSprite->spr, 0.0f, 0.0f);
 	C2D_SpriteSetPos(&introSprite->spr, 52, 0);
-
-
-	//sprite->dx = rand()*4.0f/RAND_MAX - 2.0f;
-	//sprite->dy = rand()*4.0f/RAND_MAX - 2.0f;
 }
 
-static int intro(int fade, u32 kDown) {
-	int msec = 500;
-	int trigger = 15;
-	time_t start, stop;
+static int intro(int fade, u32 kDown, bool introFlag) {
+	/*int mstime = 15000;
+	clock_t start_time = clock();
+			int i = 0;
 
-	start = time(NULL);
-
-	if(kDown & KEY_LEFT) {
-			
-	}
-
-	//Decrement fade counter
-	if(fade > 0) {
-		return fade--;
-	}
-
-	//Increment fade counter
-	/*if(fade < 255) {
-		return fade++;
+	while(clock() > mstime) {
+		if (kDown & KEY_UP) {
+			i++;
+			printf("\x1b[9;1H%d", i);}
+			//break;
 	}*/
 
 	C2D_Fade(C2D_Color32(0, 0, 0, fade)); //255 blank, 0 visible
 	C2D_DrawSprite(&sprites[0].spr);
+
+	//Decrement fade counter
+	if((fade > 0) && (introFlag == true)) {
+		fade--;
+	}
+
+	if((fade < 255) && (introFlag == false)) {
+		fade++;
+	}
+
+	printf("\x1b[8;1H%d", fade);
+
+	return fade;
 }
 
 //---------------------------------------------------------------------------------
@@ -86,6 +86,7 @@ int main(int argc, char* argv[]) {
 	initSprites();
 
 	int fade = 255;
+	bool introFlag = true;
 
 	// Main loop
 	while (aptMainLoop())
@@ -94,25 +95,8 @@ int main(int argc, char* argv[]) {
 
 		// Respond to user input
 		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-
-		u32 kHeld = hidKeysHeld();
-		/*if(kHeld & KEY_UP) {
-			if(fade < 255) {
-				fade++;
-			}
-		}
-		if(kHeld & KEY_DOWN) {
-			if(fade > 0) {
-				fade--;
-			}
-		}*/
-
-
+		
 		// Image shown for 15 seconds, with about half a second transition time, audio is played first then image is shown.
-
-
 
 		printf("\x1b[1;1HSprites: %zu/%u\x1b[K", numSprites, MAX_SPRITES);
 		printf("\x1b[2;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
@@ -124,11 +108,12 @@ int main(int argc, char* argv[]) {
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, C2D_Color32f(0.0f, 0.0f, 0.0f, 1.0f));
 		C2D_SceneBegin(top);
-		fade = intro(fade, kDown);
+		fade = intro(fade, kDown, introFlag);
+		if((kDown & KEY_A) && (fade == 0)) {
+			introFlag = false;
+		}
 
 		C3D_FrameEnd(0);
-
-		printf("\x1b[8;1H%d", fade);
 	}
 
 	// Delete graphics
